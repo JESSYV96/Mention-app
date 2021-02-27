bee41bcbf8261f2e85b1d06712bbdb473a39f624
jessyv96 <c.j91350@gmail.com>
2021-02-27 19:44:03 +0100

    Fetch Mentions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import axios, { AxiosRequestConfig } from 'axios';
 import Post from './components/Post/Post'
 import './App.css';
 
 function App() {
-  const [state, setstate] = useState('');
+  const [mentionsPosts, setMentionsPosts] = useState([])
+    
+  const options: AxiosRequestConfig = useMemo(() => {
+   return {
+      method: 'get',
+      headers: {
+        'Authorization': `Bearer ${process.env.REACT_APP_TOKEN_ACCESS}`,
+        'Access-Control-Allow-Methods': "DELETE, POST, GET, OPTIONS",
+      }
+    }
+  }, [])
+  
+  const fetchMentions = useCallback(() =>  {
+    axios.get(`http://localhost:8081/api/accounts/${process.env.REACT_APP_MENTION_ACCOUNT_ID}/alerts/${process.env.REACT_APP_ALERT_ID}/mentions`, options)
+      .then(response => {
+        return setMentionsPosts(response.data.mentions)
+      })
+      .catch(err => console.log(err))
+  },[options])
 
-
-  // useEffect(() => {
-  //  fetch(`https://web.mention.com/api/accounts/{account_id}/alerts/{alert_id}/mentions`)
-  // }, []);
-
+  useEffect(() => {
+    fetchMentions();
+  }, [fetchMentions]);
 
   return (
     <div className="App">
-        <Post/>
+      {mentionsPosts !== [] && (
+        mentionsPosts.map(mention => {
+          return <Post key={mention.id} mention={mention} />
+        })
+      )}
     </div>
   );
 }
--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
-const Post = () => {
+const Post = ({ mention }: any) => {
+    const [isMentionRead, setIsMentionRead] = useState(mention.read)
+
+    const underlineKeyword = (text: string, keyword: string) => {
+
+    }
+
+    const formatStringDate = (stringDate: string): String => {
+        const dateformatted: Date = new Date(stringDate);
+        const month = new Intl.DateTimeFormat('en-US', { month: "short" }).format(dateformatted);
+        return `${dateformatted.getDate()} ${month}`
+    }
+
     return (
-        <div className="post">
+        <div className="post" onClick={() => setIsMentionRead(!isMentionRead)}>
             <div className="avatarContainer">
-                <img 
-                className="postAvatar"
-                src="https://picsum.photos/seed/200/300.jpg" 
-                alt="avatar"/>
+                <img
+                    className="postAvatar"
+                    src="https://picsum.photos/seed/200/300.jpg"
+                    alt="avatar" />
                 <span className="categoryPostIcon" />
-                <div className="noReadMark"></div>
+                <div className={!isMentionRead ? 'noReadMark' : ''}></div>
             </div>
             <div className="postDetailsContainer">
                 <div className="postDetailsHead">
-                    <span className="textDetails">patheos.com</span>
-                    <span className="textDetails">28 Feb</span>
+                    <span className="textDetails">{mention.source_name}</span>
+                    <span className={!isMentionRead ? 'noReadTextDetails' : 'textDetails'}>{formatStringDate(mention.created_at)}</span>
                 </div>
                 <div className="postDetailsBody">
-                    <span className="titleText">"Why the "ugh" - I didn't claim t...</span>
-                    <span className="descriptionText"> 
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Obcaecati laudantium amet nemo vitae fugiat!
+                    <span className="titleText">{mention.title.length > 35 ? `${mention.title.substring(0, 30)} ...` : mention.title}</span>
+                    <span className="descriptionText">
+                        {mention.description.length > 80 ? `${mention.description.substring(0, 80)} ...` : mention.description}
                     </span>
                 </div>
             </div>
